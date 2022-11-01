e54b55eb42cbf851ffbb73be99825be96e1eda97
h4x4d <cleanyboom@gmail.com>
2022-11-01 21:37:44 +0300

    hw 3 validator fix

--- intensive_django/catalog/validators.py
+++ intensive_django/catalog/validators.py
@@ -20,7 +20,7 @@ def validate_must_be_param(*args):
 
     @wraps(validate_must_be_param)
     def _inner(value):
-        check_value = value.lower()
+        check_value = re.sub(r'[^\w\s]', ' ', value.lower())
 
         if len(args & set(check_value.split())) == 0:
             raise ValidationError(
