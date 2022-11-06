1ee586f20d1eef3e3486bee2533c0129c687848e
h4x4d <cleanyboom@gmail.com>
2022-11-06 11:25:51 +0300

    HW-4 add  preview GalleryImage and MainImage

--- intensive_django/catalog/admin.py
+++ intensive_django/catalog/admin.py
@@ -13,7 +13,6 @@ class AdminItem(admin.ModelAdmin):
     filter_horizontal = ('tags', )
 
     def get_img(self, obj):
-        print(self)
         return get_thumbnail(obj.mainimage.image, "300x300", crop='center',
                              qualify=51)
 
@@ -28,5 +27,43 @@ class AdminItem(admin.ModelAdmin):
 
 admin.site.register(Tag)
 admin.site.register(Category)
-admin.site.register(MainImage)
-admin.site.register(GalleryImage)
+
+
+@admin.register(MainImage)
+class AdminMainImage(admin.ModelAdmin):
+    list_display = ('img_name', 'img_thumb',)
+
+    def get_img(self, obj):
+        return get_thumbnail(obj.image, "300x300", crop='center',
+                             qualify=51)
+
+    def img_thumb(self, obj):
+        if obj.item:
+            return mark_safe(f'<img src="{self.get_img(obj).url}">')
+        return "Нет изображения"
+
+    def img_name(self, obj):
+        return obj.item.name
+
+    img_thumb.short_description = 'Превью'
+    img_thumb.allow_tags = True
+
+
+@admin.register(GalleryImage)
+class AdminGalleryImage(admin.ModelAdmin):
+    list_display = ('img_name', 'img_thumb',)
+
+    def get_img(self, obj):
+        return get_thumbnail(obj.image, "300x300", crop='center',
+                             qualify=51)
+
+    def img_thumb(self, obj):
+        if obj.item:
+            return mark_safe(f'<img src="{self.get_img(obj).url}">')
+        return "Нет изображения"
+
+    def img_name(self, obj):
+        return obj.item.name
+
+    img_thumb.short_description = 'Превью'
+    img_thumb.allow_tags = True
