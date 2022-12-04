4aa013ce32e2b2fd5cc8e5ee2f51e10d1b7fb02c
h4x4d <cleanyboom@gmail.com>
2022-12-04 19:17:55 +0300

    HW-8 | Fix 404 View

--- intensive_django/Core/views.py
+++ intensive_django/Core/views.py
@@ -3,4 +3,9 @@ from django.views.generic import TemplateView
 
 class NotFoundView(TemplateView):
     template_name = 'errors/404.html'
-    status_code = 404
+
+    def get(self, request, *args, **kwargs):
+        response = super(NotFoundView, self).get(request, *args, **kwargs)
+        response.status_code = 404
+
+        return response
