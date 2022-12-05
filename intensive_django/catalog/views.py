b6f79c1b330e6dcac908803fc610a4df5764620f
Maxim Sharkevich <67413544+SharkyJunior@users.noreply.github.com>
2022-12-05 11:01:02 +0300

    removed unused import

--- intensive_django/catalog/views.py
+++ intensive_django/catalog/views.py
@@ -1,5 +1,4 @@
 from catalog.models import Item
-from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import DetailView, FormView, ListView
 from rating.forms import SetRatingForm
